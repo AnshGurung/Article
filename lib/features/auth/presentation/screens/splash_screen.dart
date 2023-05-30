6d0b13382f038791d9da3df524917b247dff9994
Your Name <you@example.com>
2023-05-30 14:02:00 +0545

    Added Splash screen

--- lib/features/auth/presentation/screens/splash_screen.dart
+++ lib/features/auth/presentation/screens/splash_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import '../../../../core/presentation/theme/colours.dart';
 import '../../controllers/slpash_controller.dart';
 
 class SplashScreen extends StatelessWidget {
@@ -8,10 +9,18 @@ class SplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    bool isLightTheme = true;
+    String imagePath = "assets/images/";
+    String lightLogo = "BYTE-light.png";
+    String darkLogo = "BYTE-dark.png";
     Get.put(SplashContoller());
-    return const Scaffold(
+    return Scaffold(
+      backgroundColor: isLightTheme ? backgroundLight : backgroundDark,
       body: Center(
-        child: Text('Splash Screen is under devlopment!'),
+        child: Image.asset(
+          imagePath + (isLightTheme ? lightLogo : darkLogo),
+          height: 150,
+        ),
       ),
     );
   }
